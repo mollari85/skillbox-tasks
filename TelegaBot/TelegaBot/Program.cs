cbd0823e252bf28fd59ebad51957ae6d4aac1571
mollari85 <melnikovdn@bk.ru>
2022-10-06 19:17:32 +0300

    telega

---
+++ TelegaBot/TelegaBot/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+
+    class Program
+    {
+       // static TelegramBotClient bot = new TelegramBotClient("5694291268:AAHywUh_dKklgXyBku9vnlOzUpeDBb1eU3c");
+
+
+        static void Main(string[] args)
+        {
+            TelegramBotClient bot = new TelegramBotClient("5694291268:AAHywUh_dKklgXyBku9vnlOzUpeDBb1eU3c");
+            bot.StartReceiving(Update,Error);
+            Console.ReadLine();
+        }
+
+        async private static Task Update(ITelegramBotClient botClient, Update update, CancellationToken arg3)
+        {
+            var message = update.Message;
+            if (message!=null)
+            if (message.Text != null)
+            {
+                if (message.Text.Contains("hi"))
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Back off");
+
+            }
+        }
+
+
+
+         private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            Console.WriteLine(arg2.Message);
+            return (null);
+            
+        }
+    }
+}
